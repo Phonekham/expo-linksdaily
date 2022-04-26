9d009f3fb6f58987224737be25f6b8133d3a5e41
Phonekham <phone.madridista@gmail.com>
2022-04-27 01:07:02 +0700

    screens

--- components/nav/FooterTabs.jsx
+++ components/nav/FooterTabs.jsx
@@ -2,36 +2,70 @@ import React, { useState } from "react";
 import { View, TouchableOpacity } from "react-native";
 import Text from "@kaloraat/react-native-text";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Divider } from "react-native-elements";
 
-export const Tab = ({ name, text }) => (
-  <TouchableOpacity>
-    <>
-      <Text>
-        <FontAwesome5
-          size={25}
-          name={name}
-          style={{ marginBottom: 3, alignSelf: "center" }}
-        />{" "}
-        {text}
-      </Text>
-    </>
-  </TouchableOpacity>
-);
+export const Tab = ({ name, text, handlePress, screenName, routeName }) => {
+  const activeScreenColor = screenName === routeName && "orange";
 
-export default function FooterTabs() {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        margin: 10,
-        marginHorizontal: 30,
-        justifyContent: "space-between",
-      }}
-    >
-      <Tab text="Home" name="home" />
-      <Tab text="Post" name="plus-square" />
-      <Tab text="Links" name="list-ol" />
-      <Tab text="Account" name="user" />
-    </View>
+    <TouchableOpacity onPress={handlePress}>
+      <FontAwesome5
+        name={name}
+        size={25}
+        style={{
+          marginBottom: 3,
+          alignSelf: "center",
+        }}
+        color={activeScreenColor}
+      />
+      <Text>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+export default function FooterTabs() {
+  const navigation = useNavigation();
+  const route = useRoute();
+  return (
+    <>
+      <Divider width={1} />
+      <View
+        style={{
+          flexDirection: "row",
+          margin: 10,
+          marginHorizontal: 30,
+          justifyContent: "space-between",
+        }}
+      >
+        <Tab
+          text="Home"
+          name="home"
+          handlePress={() => navigation.navigate("Home")}
+          screenName="Home"
+          routeName={route.name}
+        />
+        <Tab
+          text="Post"
+          name="plus-square"
+          handlePress={() => navigation.navigate("Post")}
+          screenName="Post"
+          routeName={route.name}
+        />
+        <Tab
+          text="Links"
+          name="list-ol"
+          handlePress={() => navigation.navigate("Links")}
+          screenName="Links"
+          routeName={route.name}
+        />
+        <Tab
+          text="Account"
+          name="user"
+          handlePress={() => navigation.navigate("Account")}
+          screenName="Account"
+          routeName={route.name}
+        />
+      </View>
+    </>
   );
 }
--- components/nav/ScreensNav.jsx
+++ components/nav/ScreensNav.jsx
@@ -6,6 +6,9 @@ import Home from "../../screens/Home";
 import Signin from "../../screens/Signin";
 import Signup from "../../screens/Signup";
 import HeaderTabs from "./HeaderTabs";
+import Account from "../../screens/Account";
+import Post from "../../screens/Post";
+import Links from "../../screens/Links";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,15 +17,29 @@ export default function ScreensNav() {
   const authenticated = state && state.token !== "" && state.user !== null;
   return (
     <Stack.Navigator
-      initialRouteName="Signin"
+      initialRouteName="Home"
       screenOptions={{ headerShown: false }}
     >
-      {authenticated ? (
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: "Links Daily", headerRight: () => <HeaderTabs /> }}
-        />
+      {!authenticated ? (
+        <>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              title: "Links Daily",
+              headerRight: () => <HeaderTabs />,
+            }}
+          />
+          <Stack.Screen
+            name="Account"
+            component={Account}
+            options={{
+              headerBackTitle: "Back",
+            }}
+          />
+          <Stack.Screen name="Post" component={Post} />
+          <Stack.Screen name="Links" component={Links} />
+        </>
       ) : (
         <>
           <Stack.Screen name="Signup" component={Signup} />
